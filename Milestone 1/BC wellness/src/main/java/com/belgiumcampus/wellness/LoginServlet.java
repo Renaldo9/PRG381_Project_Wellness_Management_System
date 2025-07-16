6139eef95d366a7838e85225cbdb4dac76022b5b
DropTheBeat04 <gabrielbotha5@gmail.com>
2025-07-16 13:35:38 +0200

    Removing unnecessary imports.

--- Milestone 1/BC wellness/src/main/java/com/belgiumcampus/wellness/LoginServlet.java
+++ Milestone 1/BC wellness/src/main/java/com/belgiumcampus/wellness/LoginServlet.java
@@ -8,12 +8,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
+
 
 
 @WebServlet("/login")
